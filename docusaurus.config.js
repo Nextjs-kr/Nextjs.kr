564abb268d86aa8c76e5029f8abd8d11603bcc9d
nano <nano@nano.seoul.kr>
2023-11-02 18:07:37 +0900

    build: 빌드 설정 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,9 @@ const config = {
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: "nano-lee", // Usually your GitHub org/user name.
 	projectName: "nano-nextjs-study", // Usually your repo name.
-	deploymentBranch: "main",
 
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
+	onBrokenLinks: "ignore",
+	onBrokenMarkdownLinks: "ignore",
 
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
