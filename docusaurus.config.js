d099859f54e3f9d615c8b27949235907c9f13243
Nano <nano@nano.seoul.kr>
2023-11-07 00:10:43 +0900

    build: trailingSlash 옵션 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,8 @@ const config = {
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "ignore",
 
+  trailingSlash: false,
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
